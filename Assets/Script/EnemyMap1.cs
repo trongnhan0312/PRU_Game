e515fa6829946dc69d4584fc29c5a9e24a542153
trongnhan0312 <nhanbin0312@gmail.com>
2025-02-13 23:37:46 +0700

    enemyMap1

--- Assets/Script/EnemyMap1.cs
+++ Assets/Script/EnemyMap1.cs
@@ -1,7 +1,66 @@
+﻿using System.Collections;
 using UnityEngine;
 
 public class EnemyMap1 : Enemy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
+    public Animator animator;
+    public float attackCooldown = 1f; // Thời gian giữa các lần gây sát thương
+    private float lastAttackTime = 0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.TakeDame(enterDamage);
+                animator.SetBool("IsAttacking", true);
+                animator.SetBool("IsMoving", false);
+            }
+        }
+    }
+
+    public float damageInterval = 0.3f; // Thời gian giữa các lần gây sát thương
+    private bool isDamaging = false; // Kiểm soát tránh gây sát thương liên tục mỗi frame
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !isDamaging)
+        {
+            StartCoroutine(DealDamageOverTime(collision.gameObject.GetComponent<PlayerController>()));
+        }
+    }
+
+    private IEnumerator DealDamageOverTime(PlayerController player)
+    {
+        isDamaging = true;
+        while (player != null && isDamaging) // Chỉ tiếp tục nếu player còn trong vùng quái
+        {
+            player.TakeDame(stayDamage);
+            Debug.Log("🔥 Gây sát thương liên tục cho Player!");
+            yield return new WaitForSeconds(damageInterval);
+        }
+        isDamaging = false;
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                StopAllCoroutines(); // Dừng tất cả coroutine đang chạy
+                player.ResetHurtAnimation(); // Gọi hàm để tắt IsHurt khi rời khỏi quái
+            }
+
+            animator.SetBool("IsAttacking", false);
+            animator.SetBool("IsMoving", true);
+        }
+    }
+
+
 }
