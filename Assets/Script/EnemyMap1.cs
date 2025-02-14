c0fd56f918f1ef41e6b75dd510b802ae199d966b
trongnhan0312 <nhanbin0312@gmail.com>
2025-02-14 14:31:45 +0700

    BaseEnemy

--- Assets/Script/EnemyMap1.cs
+++ Assets/Script/EnemyMap1.cs
@@ -6,7 +6,8 @@ public class EnemyMap1 : Enemy
     public Animator animator;
     public float attackCooldown = 1f; // Thời gian giữa các lần gây sát thương
     private float lastAttackTime = 0f;
-
+    [SerializeField] private GameObject manaObject;
+    [SerializeField] private GameObject effectObject;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -60,6 +61,34 @@ public class EnemyMap1 : Enemy
             animator.SetBool("IsAttacking", false);
             animator.SetBool("IsMoving", true);
         }
+    }
+
+    protected override void Die()
+    {
+        if (manaObject != null)
+        {
+            Vector3 spawnPosition = transform.position;
+
+            // Raycast kiểm tra vị trí mặt đất
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, LayerMask.GetMask("Ground"));
+            if (hit.collider != null)
+            {
+                spawnPosition.y = hit.point.y + 0.05f; // Đặt item hơi cao một chút để tránh bị lọt vào nền
+            }
+            Vector3 effect = transform.position;
+
+            // Raycast kiểm tra vị trí mặt đất
+            RaycastHit2D hits = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, LayerMask.GetMask("Ground"));
+            if (hit.collider != null)
+            {
+                effect.y = hit.point.y + 0.8f; // Đặt effect hơi cao một chút để tránh bị lọt vào nền
+            }
+            GameObject effectDie = Instantiate(effectObject, effect, Quaternion.identity);
+            Destroy(effectDie, 0.5f);
+            GameObject mana = Instantiate(manaObject, spawnPosition, Quaternion.identity);
+            Destroy(mana, 10f);
+        }
+        base.Die();
     }
 
 
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,207 +1,246 @@
-﻿using System.Collections;
-using Unity.Mathematics;
-using UnityEngine;
-using UnityEngine.UI;
+﻿    using System.Collections;
+    using Unity.Mathematics;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
-{
-
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 15f;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Transform groundCheck;
-    private Animator animator;
-    private bool isGrounded;
-    private Rigidbody2D rb;
-
-
-    [SerializeField] private Transform firePos;
-    [SerializeField] private GameObject bulletPrefabs;
-    [SerializeField] private float shotDelay=0.15f;
-    private float nextShot;
-    [SerializeField] private int maxAmmo = 24;
-    public int currentAmmo;
-
-    [SerializeField] private Transform attackPoint;
-    [SerializeField] private float attackRange = 1f;
-    [SerializeField] private LayerMask enemyLayers;
-
-
-    /* private GameManager gameManager;*/
-
-    [SerializeField] private Image ammoBar; // Thanh màu xanh (UI Image)
-    [SerializeField] protected float maxHp = 100f;
-    protected float currentHp;
-    [SerializeField] private Image hpBar;
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-/*        gameManager = FindAnyObjectByType<GameManager>();*/
-    }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        currentAmmo=maxAmmo;
-        currentHp = maxHp;
-        UpdateHpBar();
-    }
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float jumpForce = 15f;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Transform groundCheck;
+        private Animator animator;
+        private bool isGrounded;
+        private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-/*        if (gameManager.IsGameOver() || gameManager.IsGameWin()) return;*/
-        HandleMovement();
-        HandleJump();
-        UpdateAnimation();
-        HandleShoot();
-        HandleAttack();
 
-    }
+        [SerializeField] private Transform firePos;
+        [SerializeField] private GameObject bulletPrefabs;
+        [SerializeField] private float shotDelay=0.15f;
+        private float nextShot;
+        [SerializeField] private int maxMana = 24;
+        public float currentMana;
 
-    private void HandleMovement()
-    {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-        if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
-        else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
-    }
+        [SerializeField] private Transform attackPoint;
+        [SerializeField] private float attackRange = 1f;
+        [SerializeField] private LayerMask enemyLayers;
 
-    private void HandleJump()
-    {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        private SpriteRenderer spriteRenderer;  
+        /* private GameManager gameManager;*/
+
+        [SerializeField] private Image manaBar; // Thanh màu xanh (UI Image)
+        [SerializeField] protected float maxHp = 100f;
+        protected float currentHp;
+        [SerializeField] private Image hpBar;
+
+        private void Awake()
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-
-    }
-    private IEnumerator ShootRoutine()
-    {
-        animator.SetTrigger("Shoot");
-
-        // Chờ đến khi animation bắn kết thúc
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-
-        // Tạo đạn
-        GameObject bullet = Instantiate(bulletPrefabs, firePos.position, firePos.rotation);
-
-        // Kiểm tra hướng nhân vật
-        if (transform.localScale.x < 0)
-        {
-            Vector3 bulletScale = bullet.transform.localScale;
-            bulletScale.x *= -1; // Lật viên đạn lại
-            bullet.transform.localScale = bulletScale;
+            animator = GetComponent<Animator>();
+            rb = GetComponent<Rigidbody2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            /*        gameManager = FindAnyObjectByType<GameManager>();*/
         }
 
-        currentAmmo--;
-        UpdateAmmoUI();
-    }
-
-    private void HandleShoot()
-    {
-        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f; // Kiểm tra chạy
-        bool isJumping = !isGrounded; // Kiểm tra nhảy
-
-        // Nếu đang chạy hoặc đang nhảy thì không thể bắn
-        if (isRunning || isJumping) return;
-
-        if (Input.GetMouseButtonDown(1) && currentAmmo > 0 && Time.time > nextShot)
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            nextShot = Time.time + shotDelay;
-            StartCoroutine(ShootRoutine());
+            currentMana= maxMana;
+            currentHp = maxHp;
+            UpdateHpBar();
         }
-    }
-    private void HandleAttack()
-    {
-        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
-        bool isJumping = !isGrounded;
-        if (isRunning || isJumping) return;
 
-        if (Input.GetMouseButtonDown(0)) // Chuột trái để đánh
+        // Update is called once per frame
+        void Update()
         {
-            animator.SetTrigger("IsAttacking");
-            Attack(); // Gọi trực tiếp Attack()
+    /*        if (gameManager.IsGameOver() || gameManager.IsGameWin()) return;*/
+            HandleMovement();
+            HandleJump();
+            UpdateAnimation();
+            HandleShoot();
+            HandleAttack();
+
         }
-    }
 
-
-
-    private void Attack()
-    {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
-
-        foreach (Collider2D enemy in hitEnemies)
+        private void HandleMovement()
         {
-            if (enemy.CompareTag("Enemy")) // Kiểm tra tag Enemy
+            float moveInput = Input.GetAxis("Horizontal");
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+            if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
+            else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        private void HandleJump()
+        {
+            if (Input.GetButtonDown("Jump") && isGrounded)
             {
-                Enemy enemyScript = enemy.GetComponent<Enemy>();
-                if (enemyScript != null)
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        }
+        private IEnumerator ShootRoutine()
+        {
+            animator.SetTrigger("Shoot");
+
+            // Chờ đến khi animation bắn kết thúc
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+            // Tạo đạn
+            GameObject bullet = Instantiate(bulletPrefabs, firePos.position, firePos.rotation);
+
+            // Kiểm tra hướng nhân vật
+            if (transform.localScale.x < 0)
+            {
+                Vector3 bulletScale = bullet.transform.localScale;
+                bulletScale.x *= -1; // Lật viên đạn lại
+                bullet.transform.localScale = bulletScale;
+            }
+
+        currentMana--;
+            UpdateAmmoUI();
+        }
+
+        private void HandleShoot()
+        {
+            bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f; // Kiểm tra chạy
+            bool isJumping = !isGrounded; // Kiểm tra nhảy
+
+            // Nếu đang chạy hoặc đang nhảy thì không thể bắn
+            if (isRunning || isJumping) return;
+
+            if (Input.GetMouseButtonDown(1) && currentMana > 0 && Time.time > nextShot)
+            {
+                nextShot = Time.time + shotDelay;
+                StartCoroutine(ShootRoutine());
+            }
+        }
+        private void HandleAttack()
+        {
+            bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
+            bool isJumping = !isGrounded;
+            if (isRunning || isJumping) return;
+
+            if (Input.GetMouseButtonDown(0)) // Chuột trái để đánh
+            {
+                animator.SetTrigger("IsAttacking");
+                animator.SetBool("IsHurt", false);
+                Attack(); // Gọi trực tiếp Attack()
+            }
+        }
+
+
+
+        private void Attack()
+        {
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                if (enemy.CompareTag("Enemy")) // Kiểm tra tag Enemy
                 {
-                    enemyScript.TakeDamage(10f); // Gây 10 sát thương
+                    Enemy enemyScript = enemy.GetComponent<Enemy>();
+                    if (enemyScript != null)
+                    {
+                        enemyScript.TakeDamage(10f); // Gây 10 sát thương
+                    }
                 }
             }
         }
-    }
 
-    private void UpdateAmmoUI()
-    {
-        if (ammoBar != null)
-            ammoBar.fillAmount = (float)currentAmmo / maxAmmo;
-    }
-
-    private void UpdateAnimation()
-    {
-        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
-        bool isJumping = !isGrounded;
-
-        animator.SetBool("IsRunning", isRunning);
-        animator.SetBool("IsJumping", isJumping);
-    }
-
-
-    public void TakeDame(float damage)
-    {
-        currentHp -= damage;
-        currentHp = Mathf.Max(currentHp, 0);
-        UpdateHpBar();
-
-        StopAllCoroutines(); // Dừng tất cả animation trước đó để tránh bị ghi đè
-        animator.SetBool("IsHurt", true); // Chạy animation bị thương
-
-        if (currentHp <= 0)
+        private void UpdateAmmoUI()
         {
-            Die();
+            if (manaBar != null)
+            manaBar.fillAmount = (float)currentMana / maxMana;
+        }
+
+        private void UpdateAnimation()
+        {
+            bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
+            bool isJumping = !isGrounded;
+
+            animator.SetBool("IsRunning", isRunning);
+            animator.SetBool("IsJumping", isJumping);
+        }
+
+
+        public void TakeDame(float damage)
+        {
+            currentHp -= damage;
+            currentHp = Mathf.Max(currentHp, 0);
+            UpdateHpBar();
+
+            StopAllCoroutines(); // Dừng tất cả animation trước đó để tránh bị ghi đè
+            animator.SetBool("IsHurt", true); // Chạy animation bị thương
+            StartCoroutine(HurtEffect());
+
+            if (currentHp <= 0)
+            {
+                Die();
+            }
+        }
+
+
+
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
+
+        private void UpdateHpBar()
+        {
+            if (hpBar != null)
+            {
+                hpBar.fillAmount = currentHp / maxHp;
+            }
+        }
+        private void OnDrawGizmosSelected()
+        {
+            if (attackPoint != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+            }
+        }
+        public void ResetHurtAnimation()
+        {
+            animator.SetBool("IsHurt", false);
+        }
+
+        // Hiệu ứng nhấp nháy đỏ
+        private IEnumerator HurtEffect()
+        {
+       
+                spriteRenderer.color = Color.red;
+                yield return new WaitForSeconds(0.1f);
+                spriteRenderer.color = Color.white;
+                yield return new WaitForSeconds(0.1f);
+        
+        }
+    // ✅ Hàm cập nhật thanh mana
+    private void UpdateManaBar()
+    {
+        if (manaBar != null)
+        {
+            manaBar.fillAmount = currentMana / maxMana;
         }
     }
 
-
-
-    private void Die()
+    // ✅ Hàm tăng mana
+    public void IncreaseMana(float amount)
     {
-        Destroy(gameObject);
+        currentMana += amount;
+        currentMana = Mathf.Min(currentMana, maxMana); // Không vượt quá giới hạn
+        UpdateManaBar(); // Cập nhật UI
     }
 
-    private void UpdateHpBar()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (hpBar != null)
+        if (collision.CompareTag("Mana"))
         {
-            hpBar.fillAmount = currentHp / maxHp;
+            Destroy(collision.gameObject);
+            IncreaseMana(0.2f * maxMana);
+            UpdateAmmoUI();// Cộng 20% Mana
         }
     }
-    private void OnDrawGizmosSelected()
-    {
-        if (attackPoint != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-        }
-    }
-    public void ResetHurtAnimation()
-    {
-        animator.SetBool("IsHurt", false);
-    }
-
 }
