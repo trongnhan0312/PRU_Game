f6b803b5cf7361cbc88df3ee3a677b15b945902d
LilMIKHuwng <nhat040104@gmail.com>
2025-03-20 00:48:51 +0700

    Fix Select Map

--- Assets/Script/NextLevelTriggerMap2.cs
+++ Assets/Script/NextLevelTriggerMap2.cs
@@ -81,21 +81,6 @@ public class NextLevelTriggerMap2 : MonoBehaviour
 
         if (UIManager != null)
         {
-            GameManager gameManager = FindObjectOfType<GameManager>();
-
-            if (gameManager != null)
-            {
-                int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-                Debug.Log($"✅ Hoàn thành màn {currentSceneIndex}, mở khóa màn tiếp theo!");
-
-                gameManager.CompleteMap(currentSceneIndex); // Gọi CompleteMap để lưu trạng thái mở khóa màn
-
-                SceneManager.LoadScene(currentSceneIndex + 1); // Chuyển sang màn tiếp theo
-            }
-            else
-            {
-                Debug.LogError("⚠️ Không tìm thấy GameManager!");
-            }
             UIManager.UnlockMap3();
             UIManager.Map(); // Gọi GameManager để mở khóa Map 2
 
