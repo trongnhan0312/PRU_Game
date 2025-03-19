59ddb20c4e82321210674e392ecf3c5e2cbbafe2
Nguyễn Thanh Tùng <nttung2k3k@gmail.com>
2025-03-19 23:01:54 +0700

    fix save map

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,23 +1,23 @@
-﻿    using UnityEngine;
-    using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-    public class GameManager : MonoBehaviour
-    {
-        private static GameManager instance;
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
 
-        private int score = 0;
+    private int score = 0;
 
-        [SerializeField] private GameObject mainMenu;
-        [SerializeField] private GameObject gameOverMenu;
-        [SerializeField] private GameObject pauseMenu;
-        [SerializeField] private GameObject MapSelection;
-        public GameObject InfoButton;
-        public GameObject guidePanel;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject gameOverMenu;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject MapSelection;
+    public GameObject InfoButton;
+    public GameObject guidePanel;
 
-        [SerializeField] private GameObject buttonMap2;
-        [SerializeField] private GameObject buttonMap3;
-        [SerializeField] private GameObject LockMap2;
-        [SerializeField] private GameObject LockMap3;
+    [SerializeField] private GameObject buttonMap2;
+    [SerializeField] private GameObject buttonMap3;
+    [SerializeField] private GameObject LockMap2;
+    [SerializeField] private GameObject LockMap3;
     public static GameManager Instance { get; private set; }
     private void Awake()
     {
@@ -35,103 +35,102 @@
 
 
     void Start()
-        {
-       
-            LockMap2?.SetActive(true);
-            LockMap3?.SetActive(true);
-            MainMenu();
-            Time.timeScale = 1f;
-        }
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                PauseMenu();
-            }
-        }
+    {
 
-        public void ToggleGuidePanel()
+      
+        MainMenu();
+        Time.timeScale = 1f;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            bool isActive = !guidePanel.activeSelf;
-            guidePanel.SetActive(isActive);
+            PauseMenu();
         }
+    }
 
-        public void AddScore(int point)
-        {
-            score += point;
-        }
+    public void ToggleGuidePanel()
+    {
+        bool isActive = !guidePanel.activeSelf;
+        guidePanel.SetActive(isActive);
+    }
 
-        public void MainMenu()
-        {
-            mainMenu?.SetActive(true);
-            gameOverMenu?.SetActive(false);
-            pauseMenu?.SetActive(false);
+    public void AddScore(int point)
+    {
+        score += point;
+    }
+
+    public void MainMenu()
+    {
+        mainMenu?.SetActive(true);
+        gameOverMenu?.SetActive(false);
+        pauseMenu?.SetActive(false);
         MapSelection.SetActive(false);
-            Time.timeScale = 0;
-        }
+        Time.timeScale = 0;
+    }
 
-        public void mapSelection()
-        {
-            MapSelection?.SetActive(true);
-            mainMenu?.SetActive(false);
-            gameOverMenu?.SetActive(false);
-            pauseMenu?.SetActive(false);
-            Time.timeScale = 0f;
-        }
+    public void mapSelection()
+    {
+        MapSelection?.SetActive(true);
+        mainMenu?.SetActive(false);
+        gameOverMenu?.SetActive(false);
+        pauseMenu?.SetActive(false);
+        Time.timeScale = 0f;
+    }
 
-        public void GameOverMenu()
-        {
-            gameOverMenu?.SetActive(true);
-            pauseMenu?.SetActive(false);
-            mainMenu?.SetActive(false);
-            Time.timeScale = 0f;
-        }
+    public void GameOverMenu()
+    {
+        gameOverMenu?.SetActive(true);
+        pauseMenu?.SetActive(false);
+        mainMenu?.SetActive(false);
+        Time.timeScale = 0f;
+    }
 
-        public void PauseMenu()
-        {
-            pauseMenu?.SetActive(true);
-            gameOverMenu?.SetActive(false);
-            mainMenu?.SetActive(false);
-            Time.timeScale = 0f;
-        }
+    public void PauseMenu()
+    {
+        pauseMenu?.SetActive(true);
+        gameOverMenu?.SetActive(false);
+        mainMenu?.SetActive(false);
+        Time.timeScale = 0f;
+    }
 
-        public void StartGame()
-        {
-            mainMenu?.SetActive(false);
-            gameOverMenu?.SetActive(false);
-            pauseMenu?.SetActive(false);
-            MapSelection?.SetActive(false);
-            Time.timeScale = 1f;
-        }
+    public void StartGame()
+    {
+        mainMenu?.SetActive(false);
+        gameOverMenu?.SetActive(false);
+        pauseMenu?.SetActive(false);
+        MapSelection?.SetActive(false);
+        Time.timeScale = 1f;
+    }
 
-        public void Map()
-        {
-            mainMenu?.SetActive(false);
-            gameOverMenu?.SetActive(false);
-            pauseMenu?.SetActive(false);
-            MapSelection?.SetActive(true);
-            Time.timeScale = 0f;
-        }
+    public void Map()
+    {
+        mainMenu?.SetActive(false);
+        gameOverMenu?.SetActive(false);
+        pauseMenu?.SetActive(false);
+        MapSelection?.SetActive(true);
+        Time.timeScale = 0f;
+    }
 
-        public void ResumeGame()
-        {
-            mainMenu?.SetActive(false);
-            gameOverMenu?.SetActive(false);
-            pauseMenu?.SetActive(false);
-            Time.timeScale = 1f;
-        }
+    public void ResumeGame()
+    {
+        mainMenu?.SetActive(false);
+        gameOverMenu?.SetActive(false);
+        pauseMenu?.SetActive(false);
+        Time.timeScale = 1f;
+    }
 
-        public void UnlockMap2()
-        {
-            buttonMap2?.SetActive(true);
-            LockMap2?.SetActive(false);
-        }
+    public void UnlockMap2()
+    {
+        buttonMap2?.SetActive(true);
+        LockMap2?.SetActive(false);
+    }
 
-        public void UnlockMap3()
-        {
-            buttonMap3?.SetActive(true);
-            LockMap3?.SetActive(false);
-        }
+    public void UnlockMap3()
+    {
+        buttonMap3?.SetActive(true);
+        LockMap3?.SetActive(false);
+    }
 
     public void OnPauseButtonClick()
     {
@@ -177,6 +176,21 @@
         {
             Debug.LogError("Không tìm thấy GameManager trong GameUI!");
         }
+    }
+
+    public void CompleteMap(int mapNumber)
+    {
+        if (mapNumber == 1)
+        {
+            PlayerPrefs.SetInt("Map2Unlocked", 1); // 1 nghĩa là đã mở khóa map 2
+            UnlockMap2(); // Gọi hàm mở khóa map 2
+        }
+        else if (mapNumber == 2)
+        {
+            PlayerPrefs.SetInt("Map3Unlocked", 1); // 1 nghĩa là đã mở khóa map 3
+            UnlockMap3(); // Gọi hàm mở khóa map 3
+        }
+        PlayerPrefs.Save(); // Lưu thay đổi vào bộ nhớ
     }
 
 
